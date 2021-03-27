56fde4de47a79318b32f4e80ff03a48575c1a23c
Brandon Martel <brandonmartel@gmail.com>
2021-03-27 11:44:23 -0500

    adding link to original issue

--- src/MultiSelect.tsx
+++ src/MultiSelect.tsx
@@ -1,45 +1,103 @@
 import {
+  HTMLChakraProps,
   Menu,
-  MenuButton,
   MenuList,
   MenuOptionGroup,
   MenuItemOption,
   MenuDivider,
-  Button,
   MenuProps,
-  InputProps
+  forwardRef,
+  useMenuButton,
+  Input,
+  MenuListProps,
+  MenuOptionGroupProps,
+  MenuItemOptionProps,
+  MenuDividerProps
 } from '@chakra-ui/react'
 import { ReactNode } from 'react'
 
-export interface MultiSelectProps
-  extends Omit<MenuProps, 'children' | 'variant' | 'size'>,
-    Omit<InputProps, 'size' | 'variant'> {
-  children?: ReactNode
+// @see https://github.com/chakra-ui/chakra-ui/issues/140
+
+export interface SelectProps
+  extends Omit<HTMLChakraProps<'select'>, 'size'>,
+    Omit<MenuProps, 'children'> {
+  defaultIsOpen?: boolean
   isLazy?: true
   closeOnSelect?: false
+  children: ReactNode
+}
+
+export interface SelectControlProps
+  extends Omit<HTMLChakraProps<'select'>, 'size'>,
+    Omit<MenuProps, 'children'> {
+  defaultIsOpen?: boolean
+  isLazy?: true
+  closeOnSelect?: false
+  children?: ReactNode
+}
+
+export interface SelectListProps extends MenuListProps {}
+export interface SelectDividerProps extends MenuDividerProps {}
+export interface SelectOptionGroupProps extends MenuOptionGroupProps {}
+export interface SelectOptionItemProps extends MenuItemOptionProps {}
+
+export const Select: React.FC<SelectProps> = (props) => {
+  return <Menu {...props} />
+}
+
+export const SelectList: React.FC<SelectListProps> = (props) => {
+  return <MenuList {...props} />
+}
+
+export const SelectOptionGroup: React.FC<SelectOptionGroupProps> = (props) => {
+  return <MenuOptionGroup {...props} />
+}
+
+export const SelectOptionItem: React.FC<SelectOptionItemProps> = (props) => {
+  return <MenuItemOption {...props} />
+}
+
+export const SelectDivider: React.FC<SelectDividerProps> = (props) => {
+  return <MenuDivider {...props} />
+}
+
+export const SelectControl = forwardRef<SelectControlProps, 'input'>(
+  (props, ref) => {
+    const { children, as: As, ...rest } = props
+
+    const inputProps = useMenuButton(rest, ref)
+
+    const Element = As || Input
+
+    return <Element {...inputProps} {...props} />
+  }
+)
+
+export interface MultiSelectProps extends Omit<SelectProps, 'children'> {
+  children?: ReactNode
 }
 
 export const MultiSelect: React.FC<MultiSelectProps> = ({
   isLazy = true,
-  closeOnSelect = false
+  closeOnSelect = false,
+  maxW,
+  ...props
 }) => {
   return (
-    <Menu closeOnSelect={closeOnSelect} isLazy={isLazy}>
-      <MenuButton as={Button} colorScheme='blue'>
-        MenuItem
-      </MenuButton>
-      <MenuList minWidth='240px'>
-        <MenuOptionGroup defaultValue='asc' title='Order' type='radio'>
-          <MenuItemOption value='asc'>Ascending</MenuItemOption>
-          <MenuItemOption value='desc'>Descending</MenuItemOption>
-        </MenuOptionGroup>
-        <MenuDivider />
-        <MenuOptionGroup title='Country' type='checkbox'>
-          <MenuItemOption value='email'>Email</MenuItemOption>
-          <MenuItemOption value='phone'>Phone</MenuItemOption>
-          <MenuItemOption value='country'>Country</MenuItemOption>
-        </MenuOptionGroup>
-      </MenuList>
-    </Menu>
+    <Select closeOnSelect={closeOnSelect} isLazy={isLazy} {...props}>
+      <SelectControl maxW={maxW} />
+      <SelectList minWidth='240px'>
+        <SelectOptionGroup defaultValue='asc' title='Order' type='radio'>
+          <SelectOptionItem value='asc'>Ascending</SelectOptionItem>
+          <SelectOptionItem value='desc'>Descending</SelectOptionItem>
+        </SelectOptionGroup>
+        <SelectDivider />
+        <SelectOptionGroup title='Country' type='checkbox'>
+          <SelectOptionItem value='email'>Email</SelectOptionItem>
+          <SelectOptionItem value='phone'>Phone</SelectOptionItem>
+          <SelectOptionItem value='country'>Country</SelectOptionItem>
+        </SelectOptionGroup>
+      </SelectList>
+    </Select>
   )
 }
